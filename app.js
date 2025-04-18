a7ecdade5f53d39471317b3914207178da13e420
Prathap <104887299+Prathaplee@users.noreply.github.com>
2025-04-18 19:44:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const referralRoutes = require('./routes/referralRoutes');
 const rateRoutes = require('./routes/rateRoutes');
 const versionRoutes = require('./routes/versionRoutes');
 const authRoutes=require('./routes/authRoutes.js')
-const routes=reqiure('./routes/routes.js')
+const routes=require('./routes/routes.js')
 const os = require('os'); // Import os module
 
 const { swaggerUi, swaggerDocs } = require('./swagger'); // Import Swagger configuration
